2e229b66ac855fa4b2844b5f3b836f0344ffd060
Yadong Jiang <ydjiang234@gmail.com>
2018-06-07 13:11:21 +0100

    1

---
+++ NACA_Class.py
@@ -0,0 +1,12 @@
+#Author: Yadong Jiang @ NUI Galway Ireland
+#Email: yadong.jiang@nuigalway.ie
+#Created Date: 7th June 2018
+#
+#Description:
+#    This is a python class which generate the NACA profiles
+
+class NACA_Profile:
+
+    def __init__(self, description='A NACA Profile'):
+        return None
+
