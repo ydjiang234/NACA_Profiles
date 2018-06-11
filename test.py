8d9ed917e1bf56a1ad378fecb06a1507f697baab
Yadong Jiang <ydjiang234@gmail.com>
2018-06-11 13:37:49 +0100

    1

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from NACA_Class import NACA_Profile, NACA4Digits, NACA5Digits
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = NACA5Digits(profileName='NACA22015', isClose=False, chordLength=1.0, twist = 0.0, description='A test profile')
+a = NACA5Digits(profileName='NACA23015', isClose=False, chordLength=1.0, twist = 0.0, description='A test profile')
 b = NACA4Digits(profileName='NACA2412', isClose=True, chordLength=1.0, twist = 0.0, description='A test profile')
 X, Y = a.getXYdata()
 plt.plot(X, Y, 'o-')
@@ -10,10 +10,10 @@ X, Y = a.getCamberXYdata()
 plt.plot(X, Y, 'o-')
 
 data = np.loadtxt('23015.txt').T
-plt.plot(data[0], data[1])
+plt.plot(data[0], data[1], 'o-')
 plt.axis('equal')
 plt.grid()
 #plt.plot(a.upperPointX,a.upperPointY)
 #plt.plot(a.lowerPointX,a.lowerPointY)
-#plt.show()
+plt.show()
 print(a.m, a.K1)
